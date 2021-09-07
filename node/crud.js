99f645732a750f03b619e2d471316528d395dec7
Amaury Gonçalves Costa <baugoncalves@gmail.com>
2021-09-07 09:10:04 -0300

    Finisshing it

--- node/crud.js
+++ node/crud.js
@@ -1,13 +1,43 @@
 const { query } = require("./connection");
+var express = require('express');
+var router = express.Router();
 
-module.exports = {
-  get: function (callback) {
+
+router.get('/', function(req, res, next) {
+    query("INSERT INTO people(name) values('Amaury')", function (error, result, field) {
+        if (error) {
+            req.json(error);
+        }
+    });
     query("select *From people", function (error, result, field) {
         if (error) {
             req.json(error);
         } else {
-            callback("success", result);
+          res.render('people-list', { title: 'People List', peopleData: result});
         }
     });
-  },
-};
+});
+
+module.exports = router;
+
+// module.exports = {
+//   get: function (callback) {
+//     query("select *From people", function (error, result, field) {
+//         if (error) {
+//             req.json(error);
+//         } else {
+//             callback("success", result);
+//         }
+//     });
+//   },
+
+//   insert: function (callback) {
+//     query("INSERT INTO people(name) values('Amaury')", function (error, result, field) {
+//         if (error) {
+//             req.json(error);
+//         } else {
+//             callback("success", result);
+//         }
+//     });
+//   },
+// };
--- node/index.js
+++ node/index.js
@@ -1,23 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+var express = require('express');
+var path = require('path');
 const crud = require("./crud");
 
+var app = express();
 
-const sql = `INSERT INTO people(name) values('Wesley')`
+const port = 3000;
 
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
-    crud.get( function (err, results) {
-      if (err == 'error') {
-          console.log(err);
-      } else {
-         console.log(results)
-      }
-    });
-})
+app.use('/', crud);
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// app.get('/', (req,res) => {
+//     crud.insert( function (err, results) {
+//         if (err == 'error') {
+//             console.log(err);
+//         }
+//     });
+
+//     res.send('<h1>Full Cycle</h1>')
+
+//     crud.get( function (err, results) {
+//       if (err == 'error') {
+//           console.log(err);
+//       } else {
+//          console.log(results)
+//       }
+//     });
+// })
 
 app.listen(port, ()=> {
     console.log('Rodando na porta ' + port)
